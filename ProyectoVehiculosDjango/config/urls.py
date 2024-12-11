5e3885beb02c3e12e4252ac31edb8ae4e58237c1
Hedy <hedy.herrada@gmail.com>
2024-12-11 09:27:53 -0300

    primera migracion

--- ProyectoVehiculosDjango/config/urls.py
+++ ProyectoVehiculosDjango/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vehiculos/', include('vehiculo.urls')),
+    path('vehiculo/', include('vehiculo.urls')),
 ]
--- ProyectoVehiculosDjango/vehiculo/models.py
+++ ProyectoVehiculosDjango/vehiculo/models.py
@@ -7,11 +7,11 @@ marcas = (('Fiat', 'Fiat'), ('Chevrolet', 'Chevrolet'), ('Ford', 'Ford'), ('Toyo
 categoria = (('particular', 'Particular'), ('carga', 'Carga'), ('transporte', 'Transporte'), ('carga', 'Carga'))
 
 class Vehiculo(models.Model):
-    marca = models.CharField(max_length=20,verbose_name='Marca',choices=marcas)
+    marca = models.CharField(max_length=20,choices=marcas)
     modelo = models.CharField(max_length=100, verbose_name='Modelo')
     serialCarroceria = models.CharField(max_length=50)
     serialMotor = models.CharField(max_length=50, verbose_name='Serial Motor')
-    categoria = models.CharField(max_length=20,verbose_name_plural='Categoria',choices=categoria)
+    categoria = models.CharField(max_length=20, choices=categoria)
     precio = models.IntegerField()
     fechaCreacion = models.DateField(auto_now_add=True)
     fechaModificacion = models.DateField(auto_now=True)
