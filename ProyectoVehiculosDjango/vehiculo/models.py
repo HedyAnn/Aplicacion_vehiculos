688c67ade57e2898bf1f1c3f9ab9a91c3583c44d
Hedy <hedy.herrada@gmail.com>
2024-12-11 09:18:46 -0300

    Se crea la aplicacion vehiculo,  modelo y clase vehiculo. Settings: se incluyo el path urls, static y templates

---
+++ ProyectoVehiculosDjango/vehiculo/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+marcas = (('Fiat', 'Fiat'), ('Chevrolet', 'Chevrolet'), ('Ford', 'Ford'), ('Toyota', 'Toyota'))
+
+categoria = (('particular', 'Particular'), ('carga', 'Carga'), ('transporte', 'Transporte'), ('carga', 'Carga'))
+
+class Vehiculo(models.Model):
+    marca = models.CharField(max_length=20,verbose_name='Marca',choices=marcas)
+    modelo = models.CharField(max_length=100, verbose_name='Modelo')
+    serialCarroceria = models.CharField(max_length=50)
+    serialMotor = models.CharField(max_length=50, verbose_name='Serial Motor')
+    categoria = models.CharField(max_length=20,verbose_name_plural='Categoria',choices=categoria)
+    precio = models.IntegerField()
+    fechaCreacion = models.DateField(auto_now_add=True)
+    fechaModificacion = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.marca} {self.modelo} {self.serialCarroceria}'
+    
